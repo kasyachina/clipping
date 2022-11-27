884c23fd95361c2df48d7eaa3b5e8d0b3bc3de71
Kasinski Nikita <kasyak2018@gmail.com>
2022-11-27 15:08:03 +0300

    add DrawLineSegments and DrawPolygon methods in PlotArea class

--- plotarea.h
+++ plotarea.h
@@ -5,11 +5,36 @@
 #include <QWidget>
 #include <vector>
 
+class LineSegmentData
+{
+public:
+    explicit LineSegmentData(int x1, int x2, int y1, int y2, const QColor& color);
+    int x1() const;
+    int x2() const;
+    int y1() const;
+    int y2() const;
+    QColor color() const;
+private:
+    int _x1, _x2, _y1, _y2;
+    QColor _color;
+};
+
+enum class PlotMode
+{
+    Segments,
+    Polygons,
+};
+
 class PlotArea : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PlotArea(QWidget *parent = nullptr);
+    explicit PlotArea(QWidget *parent = nullptr, PlotMode mode = PlotMode::Segments);
+    void AddLineSegment(const LineSegmentData& data);
+    void AddPolygonPoint(int x, int y);
+    void SetPolygonFillingColor(const QColor& color);
+    void SetPolygonBorderColor(const QColor& color);
+    void ChangeMode(PlotMode newMode);
     void Clear();
     void SetUnit(int nu);
     int getUnit() const;
@@ -23,6 +48,11 @@ private:
     int pixel_width = 1;
     int zx = 0;
     int zy = 0;
+    PlotMode mode = PlotMode::Segments;
+    std::vector<LineSegmentData> segments;
+    std::vector<std::pair<int,int>> polygonData;
+    QColor polygonFillingColor;
+    QColor polygonBorderColor;
     QColor axisColor = Qt::black;
     QColor gridColor = Qt::gray;
     QColor boxColor = Qt::gray;
@@ -31,6 +61,8 @@ private:
     void inline drawAxis(QPainter& p);
     void inline drawTicks(QPainter& p);
     void inline drawArrows(QPainter& p);
+    void inline drawLineSegments(QPainter& p);
+    void inline drawPolygon(QPainter& p);
     void paintEvent(QPaintEvent* event) override;
 };
 
