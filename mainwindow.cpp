09a657abf42349a6afe848e020620b36233e284e
Kasinski Nikita <kasyak2018@gmail.com>
2022-11-27 14:08:09 +0300

    add plotarea class to display objects on the coordinate grid

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setStyleSheet("background-color: white;");
+    QGridLayout *g = new QGridLayout(centralWidget());
+    area = new PlotArea();
+    g -> addWidget(area, 0, 0, 4, 4);
+    centralWidget()->setLayout(g);
+    setMinimumSize({600, 600});
+    setWindowTitle("Отсечения");
 }
 
 MainWindow::~MainWindow()
